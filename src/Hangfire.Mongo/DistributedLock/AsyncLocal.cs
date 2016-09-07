268f67006c81258c5c57f77a0115fff36ff61b4c
Alexey Skalozub <pieceofsummer@gmail.com>
2016-09-08 02:42:34 +0300

    Add async-aware & event-based MongoDistributedLock implementation, replace incorrect unit test with a more accurate one

---
+++ src/Hangfire.Mongo/DistributedLock/AsyncLocal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangfire.Mongo.DistributedLock
+{
+#if NET45
+
+    using System.Runtime.Remoting.Messaging;
+
+    internal class AsyncLocal<T>
+    {
+        private readonly string __name;
+
+        public AsyncLocal()
+        {
+            __name = Guid.NewGuid().ToString();
+        }
+
+        public T Value
+        {
+            get
+            {
+                var value = CallContext.LogicalGetData(__name);
+                return value == null ? default(T) : (T)value;
+            }
+            set
+            {
+                CallContext.LogicalSetData(__name, value);
+            }
+        }
+    }
+
+#endif
+}
